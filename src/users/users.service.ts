99cb90eb4211ef03cb85b8d7c6bdd43ae5ac3efd
askar200018 <imangali950@gmail.com>
2022-03-18 18:15:50 +0600

    41: JWT Module part One

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,6 +6,7 @@ import * as jwt from 'jsonwebtoken';
 import { CreateAccountInput } from './dtos/create-account.dto';
 import { LoginInput } from './dtos/login.dto';
 import { User } from './entities/user.entity';
+import { JwtService } from 'src/jwt/jwt.service';
 
 @Injectable()
 export class UsersService {
@@ -13,7 +14,10 @@ export class UsersService {
     @InjectRepository(User)
     private readonly users: Repository<User>,
     private readonly config: ConfigService,
-  ) {}
+    private readonly jwtService: JwtService,
+  ) {
+    this.jwtService.hello();
+  }
 
   async createAccount({
     email,
