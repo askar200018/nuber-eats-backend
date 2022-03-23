6b798a4b31b7688bcf2bcd0d72ecce2f51a3bc83
askar200018 <imangali950@gmail.com>
2022-03-23 23:32:26 +0600

    62: Mailgun API

---
+++ src/mail/ mail.service.ts
@@ -0,0 +1,34 @@
+import { Inject, Injectable } from '@nestjs/common';
+import got from 'got';
+import * as FormData from 'form-data';
+
+import { CONFIG_OPTIONS } from 'src/common/common.constants';
+import { MailModuleOptions } from './mail.interfaces';
+
+@Injectable()
+export class MailService {
+  constructor(@Inject(CONFIG_OPTIONS) private options: MailModuleOptions) {
+    // this.sendEmail('test', 'testing');
+  }
+
+  private async sendEmail(subject: string, content: string) {
+    const form = new FormData();
+    form.append('from', `Excited User <mailgun@${this.options.domain}>`);
+    form.append('to', 'askar200018@mail.ru');
+    form.append('subject', subject);
+    form.append('text', content);
+
+    const response = await got(
+      `https://api.mailgun.net/v3/${this.options.domain}/messages`,
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Basic ${Buffer.from(
+            `api:${this.options.apiKey}`,
+          ).toString('base64')}`,
+        },
+        body: form,
+      },
+    );
+  }
+}
