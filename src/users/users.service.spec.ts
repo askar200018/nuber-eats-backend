3f046c3975a3dd2371f905e6dd071b726c2edcd3
askar200018 <imangali950@gmail.com>
2022-03-27 14:00:23 +0600

    75: editProfile Test part One

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -196,6 +196,46 @@ describe('UsersService', () => {
       expect(result).toEqual({ ok: false, error: 'User not found' });
     });
   });
-  describe('editProfile', () => {});
+  describe('editProfile', () => {
+    it('should chane email', async () => {
+      const oldUser = {
+        email: 'bs@old.com',
+        verified: true,
+      };
+      const editProfileArgs = {
+        userId: 1,
+        input: {
+          email: 'bs@new.com',
+        },
+      };
+      const newVerification = {
+        code: 'code',
+      };
+      const newUser = {
+        email: editProfileArgs.input.email,
+        verified: false,
+      };
+
+      usersRepository.findOne.mockResolvedValue(oldUser);
+      verificationRepository.create.mockReturnValue(newVerification);
+      verificationRepository.save.mockResolvedValue(newVerification);
+
+      await service.editProfile(editProfileArgs.userId, editProfileArgs.input);
+
+      expect(usersRepository.findOne).toHaveBeenCalledWith(
+        editProfileArgs.userId,
+      );
+
+      expect(verificationRepository.create).toHaveBeenCalledWith({
+        user: newUser,
+      });
+      expect(verificationRepository.save).toHaveBeenCalledWith(newVerification);
+
+      expect(mailService.sendVerificationEmail).toHaveBeenCalledWith(
+        newUser.email,
+        newVerification.code,
+      );
+    });
+  });
   it.todo('verifyEmail');
 });
